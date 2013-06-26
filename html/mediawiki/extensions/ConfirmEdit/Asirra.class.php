1aedabf0ed78c634c66e17052470d42edaf017c8
Brian Lee <brianalee@gmail.com>
2013-06-26 13:21:43 -0400

    Reducing captcha timeout level to reduce spam on our wiki.

--- html/mediawiki/extensions/ConfirmEdit/Asirra.class.php
+++ html/mediawiki/extensions/ConfirmEdit/Asirra.class.php
@@ -78,7 +78,7 @@ class Asirra extends SimpleCaptcha
 		$ch = curl_init();
 		curl_setopt($ch, CURLOPT_URL, $url);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 50);
+		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
 		$resultXml = curl_exec($ch);
 		curl_close($ch);
 
