eef025f7e2a5878b941253b4773cc6930317362b
Greg Ledbetter <gsledbetter@gmail.com>
2013-07-18 15:45:04 -0400

    Links now point one directory up for ipa file.

--- html/applab/wisqars.php
+++ html/applab/wisqars.php
@@ -50,7 +50,7 @@ ob_start();
 <img src="images/WISQARSMobileApp144.png" alt="WISQARS" title="WISQARS" />
 
 </div><!--end of large_icon-->
-<div id="download_detail"><a id="wisqars-applab-download" href="../applab/downloads/wisqars/0.2.5001/WisqarsMobile_v25.ipa" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('Image10','','images/view_itunes_smaller_hover.png',1)"><img src="images/view_itunes_smaller.png" alt="View in iTunes" title="View in iTunes" name="Image10" width="91" height="20" border="0" id="Image10" /></a></div><!--end of download_detail-->
+<div id="download_detail"><a id="wisqars-applab-download" href="../applab/downloads/wisqars/WisqarsMobile.ipa" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('Image10','','images/view_itunes_smaller_hover.png',1)"><img src="images/view_itunes_smaller.png" alt="View in iTunes" title="View in iTunes" name="Image10" width="91" height="20" border="0" id="Image10" /></a></div><!--end of download_detail-->
 <div id="stats">
 
 <strong>Category:</strong>Medical<br/>
