dd52327c7fe4d129adaaa0669ecf5d1bb311e42b
Sara Brown <sara7bella@gmail.com>
2014-04-04 09:16:08 -0400

    Added support page to MMWR Express

---
+++ html/support/mmwr_express/index.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+<?php 
+if(strstr($_SERVER['HTTP_USER_AGENT'],'iPhone'))
+{
+	header('Location: index_iphone.html');
+	exit();
+}
+?>
+
+<title>MMWR Express Support | CDC Mobile Application</title>
+<link rel="stylesheet" href="styles.css" type="text/css" />
+<script type="text/javascript">
+
+ var _gaq = _gaq || [];
+ _gaq.push(['_setAccount', 'UA-23539639-1']);
+ _gaq.push(['_trackPageview']);
+
+ (function() {
+   var ga = document.createElement('script'); ga.type =
+'text/javascript'; ga.async = true;
+   ga.src = ('https:' == document.location.protocol ? 'https://ssl' :
+'http://www') + '.google-analytics.com/ga.js';
+   var s = document.getElementsByTagName('script')[0];
+s.parentNode.insertBefore(ga, s);
+ })();
+    </script>
+</head>
+
+<body>
+
+
+<div id="wrap">
+<div id="topper">
+</div><!--end of topper-->
+<div id="topline">
+</div><!--end of topline-->
+<div id="nextline">
+</div><!--end of nextline-->
+
+<div id="header">
+<a href="http://www.cdc.gov"><img src="images/cdc_logo.png" title="CDC logo" alt="CDC logo" border="0" width="141px" height"88px" /></a>
+<span style="float:right; padding-right:2%; "><img src="images/head_branding_mmwr.png" title="A product of CDC's MMWR publication, powered by Informatics R&D Laboratory" alt="A product of CDC's MMWR publication, powered by Informatics R&D Laboratory" border="0" width="674px" height"88px"/></span>
+</div><!--end of header-->
+
+<div id="page_title">
+<img src="images/page_title.png" title="MMWR Express Support" alt="MMWR Express Support" border="0" width="309px" height"74px" />
+
+</div><!--end of page title-->
+
+<div id="divider">
+</div><!--end of divider-->
+
+<div id="about">
+<img src="images/about_head.png" title="About" alt="About" border="0" width="108px" height"29px" />
+</div><!--end of about-->
+
+
+<div id="about_content">
+<p>
+<img src="images/screen_wider.png" title="MMWR Express screen shot" alt="MMWR Express screen shot" border="0" width="439px" height="368px" align="right" />
+This mobile prototype application, MMWR Express, provides fast access to the blue summary boxes in MMWR's Weekly Report.  
+ <br/><br/>
+The Morbidity and Mortality Weekly Report (MMWR) series is prepared by the Centers for Disease Control and Prevention (CDC). Often called “the voice of CDC,” the MMWR series is the agency’s primary vehicle for scientific publication of timely, reliable, authoritative, accurate, objective, and useful public health information and recommendations. 
+ <br/><br/>
+Summaries are viewable by specific MMWR article, or by searching for a specific subject (e.g., salmonella).  
+<br/><br/>
+This application is a result of a collaboration between CDC's MMWR staff and the Informatics Innovation Unit staff within the Office of Public Health Scientific Services.  
+
+
+</p>
+</div><!--end of about_content-->
+<!--<div id="screen"><img src="images/screen.png" title="PTT Advisor screen shot" alt="PTT Advisor screen shot" border="0" width="199px" height="431px" />
+</div><!--end of screen-->
+
+<div id="contact">
+<img src="images/contact_head.png" title="Contact Us" alt="Contact Us" border="0" width="158px" height"29px" />
+</div><!--end of contact-->
+
+<div id="contact_content">
+<p>For content-related questions, please visit the CDC site:  <a href="http://www.cdc.gov/mmwr">www.cdc.gov/mmwr</a>
+<br/><br/>
+For questions regarding the application (design, new features, etc.), please contact the staff at <a href="mailto:informaticslab@cdc.gov">InformaticsLab@cdc.gov</a>. <br/><br/>For more information on the Informatics R&D Lab: <a href="http://www.phiresearchlab.org">www.phiresearchlab.org</a>
+</p>
+
+
+</div><!--end of contact_content-->
+
+
+<div id="faqs">
+<img src="images/faqs_head.png" title="FAQs" alt="FAQs" border="0" width="92px" height"29px" />
+</div><!--end of faqs-->
+
+<div id="faqs_content">
+<br/>
+<span class="hang"><span class="qa">Q:</span> How do I get the MMWR Express app?</span>
+<br/>
+<span class="hang"><span class="qa">A:</span> You can <a href="#">download the app</a> for your iPhone, iPod touch, or iPad from Apple's App Store — free of charge. </span>
+<br/><br/>
+<span class="hang"><span class="qa">Q:</span> Is any information about me collected or stored on the app?</span><br/>
+<span class="hang"><span class="qa">A:</span>  No</span>
+
+<br/><br/>
+<span class="hang"><span class="qa">Q:</span> How is the "Articles" tab different from the "Search" tab?</span><br/> 
+<span class="hang"><span class="qa">A:</span> The "Articles" tab allows access to an article's blue summary box information by specific MMWR article. Articles are grouped by MMWR issue, and issues are listed in sequential order (most recent publication date first). The "Search" tab allows users to search for blue summary box information via keywords related to a subject (e.g., influenza).</span>
+<br/><br/>
+<span class="hang"><span class="qa">Q:</span> What if I want to view the full article, not just the blue summary box information?</span><br/> 
+<span class="hang"><span class="qa">A:</span> Navigate to the list of articles (either via the "Articles" tab, or by keyword via the "Search" tab). There will be a "More Info" icon (i) next to each article. Tap it, and a popover (on the iPad) or a screen (on the iPhone) will appear. Tap the "View Full Article" button on the popover/screen. It will present the full article text.</span>
+
+<br/><br/>
+<span class="hang"><span class="qa">Q:</span> How do I get the most recent MMWR issue(s) and subject information?</span><br/> 
+<span class="hang"><span class="qa">A:</span> On the "Articles" tab, navigate to the list of articles. Pull down (with your finger) to refresh the content. You will see a spinning graphic while the new content is loading. </span>
+<br/><br/>
+
+
+
+
+
+</div><!--end of faqs_content-->
+
+<div id="divider">
+</div><!--end of divider-->
+
+<div id="footer_line">
+</div><!--end of footer_line-->
+<div id="footer">
+<img src="images/cdc_info.png" title="Centers for Disease Control and Prevention, 1600 Clifton Rd., Atlanta, GA 30333, USA" alt="Centers for Disease Control and Prevention, 1600 Clifton Rd., Atlanta, GA 30333, USA" border="0" width="289px" height"35px"/>
+</div><!--end of footer-->
+
+
+</div><!--end of wrap-->
+
+
+
+
+
+</body>
+</html>
