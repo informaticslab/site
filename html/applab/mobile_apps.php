a9c7185cf484f430b56fe4f3390ea558ada9465d
Greg Ledbetter <gsledbetter@gmail.com>
2014-04-18 10:13:38 -0400

    Adding version 0.10.18.6.

--- html/applab/mobile_apps.php
+++ html/applab/mobile_apps.php
@@ -77,8 +77,8 @@ $pedigree_manifest_link = "$ios_manifest_prefix$server_domain/$pedigree_app_path
 $pedigree_ipa_path = "../$pedigree_app_path/$pedigree_ipa";
 
 # Photon Universal App  settings
-$photon_version = '0.10.17.1';
-$photon_release_date = '4/17/14';
+$photon_version = '0.10.18.6';
+$photon_release_date = '4/18/14';
 $photon_size = '2.7MB';
 $photon_ipa = 'photon.ipa';
 $photon_app_path = "applab/downloads/photon/$photon_version";
