a0a0cc1b03817d9a5596393fb84b8d9b247a81b8
Greg Ledbetter <gsledbetter@gmail.com>
2014-05-21 14:59:27 -0400

    Fixed typo.

--- html/applab/stat_calc.php
+++ html/applab/stat_calc.php
@@ -145,7 +145,6 @@ Future development plans include adding other Epi Info desktop features such as 
     <div id="screen_row"><img src="images/epi-iphone-large-7.png" alt="screenshot of Epi Info statistical calculators" title="screenshot of Epi Info statistical calculators"/></div>
     <div id="screen_row"><img src="images/epi_screen1.png" alt="screenshot of Epi Info statistical calculators" title="screenshot of Epi Info statistical calculators"/></div>
 
-    http://edemo.phiresearchlab.org/applab/images/epi_screen1.png
 </div><!--end of screenshots-->
 
 
