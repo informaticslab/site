33e7af753c17d761e79cabdd066428498f2fd261
Greg Ledbetter <gsledbetter@gmail.com>
2014-08-26 18:46:34 -0400

    Even more changes to reflect move from STD1 to STD4 (Project Lydia).

--- html/applab/lydia_ipad.php
+++ html/applab/lydia_ipad.php
@@ -83,7 +83,7 @@ or later<br/>
 <div id="second_column">
 <div id="top_right_links">
 <a href="mailto:informaticslab@cdc.gov">Contact us ></a><br/>
-<a href="https://github.com/informaticslab/lydia">Get source code ></a>
+<a href="https://github.com/informaticslab/lydia-ios">Get source code ></a>
 </div><!--end of top_right_links-->
 
 <div id="text_block">
--- html/applab/lydia_iphone_retina.php
+++ html/applab/lydia_iphone_retina.php
@@ -46,7 +46,7 @@ ob_start();
 
 
 <div id="other_links_iphone"><a href="mailto:informaticslab@cdc.gov">Contact us ></a><br/>
-<a href="https://github.com/informaticslab/lydia">Get source code ></a>
+<a href="https://github.com/informaticslab/lydia-ios">Get source code ></a>
 
 </div>
 
