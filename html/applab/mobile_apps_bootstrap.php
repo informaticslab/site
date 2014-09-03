dd4dc51d00e638d4e9ddf0499e6b5b6c62399314
Greg Ledbetter <gsledbetter@gmail.com>
2014-09-03 15:16:46 -0400

    Updated rest of projects.

--- html/applab/mobile_apps_bootstrap.php
+++ html/applab/mobile_apps_bootstrap.php
@@ -117,7 +117,6 @@ class Project {
         $this->name = $name;
         $this->app_title = $title;
         $this->short_description = $short_desc;
-        $this->long_description = $long_desc;
         $this->icon = $icon;
         $this->download_path = DOWNLOADS_PATH_ROOT.$name;
 
@@ -204,10 +203,10 @@ class Project {
 
 # PTT Advisor App
 $ptt_short_desc = 'Assists clinical providers in their evaluation of patients with an abnormal clinical laboratory blood test, specifically an abnormal PTT (Partial Thromboplastin Time).';
-$ptt_project = new Project('ptt', 'PTT Advisor', $ptt_short_desc, 'images/ptt_icon.png');
+$ptt_project = new Project('ptt-advisor', 'PTT Advisor', $ptt_short_desc, 'images/ptt_icon.png');
 
 $ptt_itunes_link = "https://itunes.apple.com/us/app/ptt-advisor/id537989131?mt=8&ls=1";
-$ptt_ios_app = new IosApp('1.0.3', '7/6/12', '1.3MB', 'PTTAdvisor.ipa', $ptt_itunes_link);
+$ptt_ios_app = new IosApp('1.0.3.001', '7/6/12', '1.3MB', 'PTTAdvisor.ipa', $ptt_itunes_link);
 $ptt_project->add_ios_app($ptt_ios_app);
 
 # Photon (MMWR Express) App  settings
@@ -221,152 +220,94 @@ $photon_project->add_ios_app($photon_ios_app);
 # Lydia settings
 $lydia_short_desc = 'Provides fast access to the blue summary boxes in MMWR\'s weekly report. Summaries are searchable by specific article, or by specific subject (e.g., salmonella). For iOS devices.';
 $lydia_project = new Project('lydia', 'STD Tx Guide 2014', $lydia_short_desc, 'images/std1_icon.png');
-
 $lydia_ios_app = new IosApp('0.1.3.3', '8/26/14', '417KB', 'StdTxGuide.ipa', null);
 $lydia_project->add_ios_app($lydia_ios_app);
-
 $lydia_android_app = new AndroidApp('0.3.1','8/26/14', '732KB', 'lydia-release.apk', null);
 $lydia_project->add_android_app($lydia_android_app);
 
-
-
-
-# end of new PHP app classes conversion
-
-
 # CLIP settings
-$clip_version = '0.5.12.001';
-$clip_release_date = '6/1/2012';
-$clip_size = '1.9MB';
-$clip_ipa = 'clipam.ipa';
-$clip_app_path = "$downloads_path/clip/$clip_version";
+$clip_short_desc = 'Created in collaboration with National Healthcare Safety Network (NHSN), this app brings the Central Line Insertion Practices Adherence Monitoring form to the iPad.';
+$clip_project = new Project('clip', 'NHSN CLIP', $clip_short_desc, 'images/clip_icon.png');
+$clip_ios_app = new IosApp('0.5.12.001', '6/1/2012', '1.9MB', 'clipam.ipa', null);
+$clip_project->add_ios_app($clip_ios_app);
 
-$clip_manifest_link = "$ios_manifest_prefix$server_domain/$clip_app_path/manifest.plist";
-$clip_ipa_path = "../$clip_app_path/$clip_ipa";
 
 # Epi Info (Stat Calc) iPad App
-$epi_info_version = '1.9';
-$epi_info_release_date = '5/14/14';
-$epi_info_size = '12.8MB';
-$epi_info_ipa = 'EpiInfo.ipa';
-$epi_info_app_path = "$downloads_path/epi/$epi_info_version";
-
-$epi_info_manifest_link = "$ios_manifest_prefix$server_domain/$epi_info_app_path/manifest.plist";
-$epi_info_ipa_path = "../$epi_info_app_path/$epi_info_ipa";
+$epi_short_desc = 'Created by CDC\'s Epi InfoTM team, this app adapts the StatCalc statistical calculators, a feature of Epi Info desktop software, for the iPad and iPhone.';
+$epi_project = new Project('epi', 'Epi Info', $epi_short_desc, 'images/epi_icon.png');
+$epi_ios_app = new IosApp('1.9', '5/14/14', '12.8MB', 'EpiInfo.ipa', null);
+$epi_project->add_ios_app($epi_ios_app);
 
 
 # NIOSH Mine Safety Sim App
-$minesim_version = '0.7301.276';
-$minesim_release_date = '6/19/2012';
-$minesim_size = '38.8MB';
-$minesim_ipa = 'mine_sim.ipa';
-$minesim_app_path = "$downloads_path/minesim/$minesim_version";
-
-$minesim_manifest_link = "$ios_manifest_prefix$server_domain/$minesim_app_path/manifest.plist";
-$minesim_ipa_path = "../$minesim_app_path/$minesim_ipa";
+$minesim_short_desc = 'Designed in collaboration with NIOSH (CDC\'s National Institute for Occupational Safety and Health), this proof-of-concept prototype trains mine workers on safety issues.';
+$minesim_project = new Project('minesim', 'NIOSH Mine Safety Training', $minesim_short_desc, 'images/mine_safety_icon.png');
+$minesim_ios_app = new IosApp('0.7301.276', '6/19/2012', '38.8MB', 'mine_sim.ipa', null);
+$minesim_project->add_ios_app($minesim_ios_app);
 
 # MMWR Map App
-$mmwr_map_version = '1.3.4.1';
-$mmwr_map_release_date = '5/5/14';
-$mmwr_map_size = '328KB';
-$mmwr_map_ipa = 'MapApp.ipa';
-$mmwr_map_app_path = "$downloads_path/mapapp/$mmwr_map_version";
-
-$mmwr_map_manifest_link = "$ios_manifest_prefix$server_domain/$mmwr_map_app_path/manifest.plist";
-$mmwr_map_ipa_path = "../$mmwr_map_app_path/$mmwr_map_ipa";
+$mmwr_map_short_desc = 'The MMWR brought to the iPad via a map-based navigation interface. The geographic areas relating to MMWR articles are indicated. There are a variety of filtering options.';
+$mmwr_map_project = new Project('mapapp', 'MMWR Map Navigator', $mmwr_map_short_desc, 'images/mmwr_map_icon.png');
+$mmwr_map_ios_app = new IosApp('1.3.4.1', '5/5/14', '328KB', 'MapApp.ipa', null);
+$mmwr_map_project->add_ios_app($mmwr_map_ios_app);
 
 # MMWR Navigator App
-$mmwr_nav_version = '0.8.11.1';
-$mmwr_nav_release_date = '5/5/14';
-$mmwr_nav_size = '10.9MB';
-$mmwr_nav_ipa = 'mmwr-navigator.ipa';
-$mmwr_nav_app_path = "$downloads_path/mmwr-navigator/$mmwr_nav_version";
+$mmwr_nav_short_desc = 'Utilizes the iPad\'s split screen interface to display MMWR content in a user-friendly way. Articles are organized into intuitive categories, making them easy to find.';
+$mmwr_nav_project = new Project('mmwr-navigator', 'MMWR Navigator', $mmwr_nav_short_desc, 'images/mmwr_nav_icon.png');
+$mmwr_nav_ios_app = new IosApp('0.8.11.1', '5/5/14', '10.9MB', 'mmwr-navigator.ipa', null);
+$mmwr_nav_project->add_ios_app($mmwr_nav_ios_app);
 
-$mmwr_nav_manifest_link = "$ios_manifest_prefix$server_domain/$mmwr_nav_app_path/manifest.plist";
-$mmwr_nav_ipa_path = "../$mmwr_nav_app_path/$mmwr_nav_ipa";
 
 # Pedigree (Family History )iPhone App settings
-$pedigree_version = '0.4.10.1';
-$pedigree_release_date = '04/15/14';
-$pedigree_size = '';
-$pedigree_ipa = 'FamilyHistory.ipa';
-$pedigree_app_path = "$downloads_path/pedigree/$pedigree_version";
-
-$pedigree_manifest_link = "$ios_manifest_prefix$server_domain/$pedigree_app_path/manifest.plist";
-$pedigree_ipa_path = "../$pedigree_app_path/$pedigree_ipa";
+$pedigree_short_desc = 'Allows users to record their family health history in one easy-to-reference, centralized place. This app makes it easy to share one\'s family health history with a clinician. For iPhone.';
+$pedigree_project = new Project('pedigree', 'Family Heath History', $pedigree_short_desc, 'images/family_hx_icon.png');
+$pedigree_ios_app = new IosApp('0.4.10.1', '4/15/14', '925KB', 'FamilyHistory.ipa', null);
+$pedigree_project->add_ios_app($pedigree_ios_app);
 
 # NIOSH Respirator App
-$respguide_version = '1.2.8.001';
-$respguide_release_date = '6/4/2012';
-$respguide_size = '321KB';
-$respguide_ipa = 'Respirator%20Guide.ipa';
-$respguide_app_path = "$downloads_path/respguide/$respguide_version";
-
-$respguide_manifest_link = "$ios_manifest_prefix$server_domain/$respguide_app_path/manifest.plist";
-$respguide_ipa_path = "../$respguide_app_path/$respguide_ipa";
-
+$respguide_short_desc = 'Built in collaboration with The National Institute for Occupational Safety and Health (NIOSH). For quickly exploring the database of NIOSH-approved particulate filtering facepiece respirators.';
+$respguide_project = new Project('respguide', 'NIOSH Facepiece Respirator Guide', $respguide_short_desc, 'images/niosh_face_icon.png');
+$respguide_ios_app = new IosApp('1.2.8.001', '6/4/2012', '321KB', 'Respirator%20Guide.ipa', null);
+$respguide_project->add_ios_app($respguide_ios_app);
 
 # Retro iPad App
-$retro_version = '0.2.1.1';
-$retro_release_date = '5/5/14';
-$retro_size = '957KB';
-$retro_ipa = 'retro.ipa';
-$retro_app_path = "$downloads_path/retro/$retro_version";
-
-$retro_manifest_link = "$ios_manifest_prefix$server_domain/$retro_app_path/manifest.plist";
-$retro_ipa_path = "../$retro_app_path/$retro_ipa";
+$retro_short_desc = 'Focuses on HIV Risk Assessment — specifically, Assessing your Risk of Contracting HIV (ARCH). This tool is the first in the ARCH suite to be delivered on a mobile platform.';
+$retro_project = new Project('retro', 'ARCH-Couples', $retro_short_desc, 'images/retro_icon.png');
+$retro_ios_app = new IosApp('0.2.1.1', '5/5/14', '957KB', 'retro.ipa', null);
+$retro_project->add_ios_app($retro_ios_app);
 
 # STD 1 settings
-$std1_version = '0.4.4.001';
-$std1_release_date = '6/4/2012';
-$std1_size = '1.73MB';
-$std1_ipa = 'Std-Guide.ipa';
-$std1_app_path = "$downloads_path/stdguide/$std1_version";
-
-$std1_manifest_link = "$ios_manifest_prefix$server_domain/$std1_app_path/manifest.plist";
-$std1_ipa_path = "../$std1_app_path/$std1_ipa";
+$std1_short_desc = 'Early mobile application prototype for CDC\'s 2010 STD Treatment Guidelines. A Reference for clinicians on the identification of and treatment regimen for STDs.';
+$std1_project = new Project('stdguide', 'STD Guide, Version 1', $std1_short_desc, 'images/std1_icon.png');
+$std1_ios_app = new IosApp('0.4.4.001', '6/4/2012', '1.73MB', 'Std-Guide.ipa', null);
+$std1_project->add_ios_app($std1_ios_app);
 
 # STD 2 settings
-$std2_version = '0.9.3.001';
-$std2_release_date = '6/4/2012';
-$std2_size = '2.36MB';
-$std2_ipa = 'STD%20Guide%202.ipa';
-$std2_app_path = "$downloads_path/std2/$std2_version";
+$std2_short_desc = 'Enhanced prototype for CDC\'s 2010 STD Treatment Guidelines. A Reference for clinicians on the identification of and treatment regimen for STDs. Version 2 has a more "portal" feel than v1.';
+$std2_project = new Project('std2', 'STD Guide, Version 2', $std2_short_desc, 'images/std2_icon.png');
+$std2_ios_app = new IosApp('0.9.3.001', '6/4/2012', '2.36MB', 'STD%20Guide%202.ipa', null);
+$std2_project->add_ios_app($std2_ios_app);
 
-$std2_manifest_link = "$ios_manifest_prefix$server_domain/$std2_app_path/manifest.plist";
-$std2_ipa_path = "../$std2_app_path/$std2_ipa";
 
 # STD 3 settings
-$std3_version = '1.0.9';
-$std3_release_date = '6/5/2013';
-$std3_size = '8.1MB';
-$std3_ipa = '';
-$std3_app_path = "$downloads_path/std3/$std3_version";
-
-$std3_itunes_link = "https://itunes.apple.com/us/app/std-tx-guide/id655206856?mt=8";
-$std3_google_play_link = "https://play.google.com/store/apps/details?id=gov.cdc.oid.nchhstp.stdguide";
-$std3_ipa_path = "../$std3_app_path/$std3_ipa";
-
+$std3_short_desc = 'The goal of this unique prototype has been to collaborate with CDC\'s STD team to design mobile apps for the iOS and Android operating systems based on the 2010 STD Treatment Guidelines.';
+$std3_project = new Project('std3', 'STD Guide, Version 3', $std3_short_desc, 'images/std3_icon.png');
+$std3_ios_app = new IosApp('1.0.9', '6/5/2013', '8.1MB', 'StdGuide3.ipa', 'https://itunes.apple.com/us/app/std-tx-guide/id655206856?mt=8');
+$std3_project->add_ios_app($std3_ios_app);
+$std3_android_app = new AndroidApp('0.3.1','8/26/14', '732KB', 'StdGuide.apk', 'https://play.google.com/store/apps/details?id=gov.cdc.oid.nchhstp.stdguide');
+$std3_project->add_android_app($std3_android_app);
 
 # Tox Guide iPhone App
-$tox_guide_version = '0.6.2.001';
-$tox_guide_release_date = '6/1/2012';
-$tox_guide_size = '254KB';
-$tox_guide_ipa = 'mToxGuide.ipa';
-$tox_guide_app_path = "$downloads_path/toxguide/$tox_guide_version";
-
-$tox_guide_manifest_link = "$ios_manifest_prefix$server_domain/$tox_guide_app_path/manifest.plist";
-$tox_guide_ipa_path = "../$tox_guide_app_path/$tox_guide_ipa";
+$tox_guide_short_desc = 'Quick reference guide provides information such as chemical and physical properties, sources of exposure, minimal risk levels, children\'s health, and health effects.';
+$tox_guide_project = new Project('toxguide', 'ATSDR ToxGuide', $tox_guide_short_desc, 'images/tox_icon.png');
+$tox_guide_ios_app = new IosApp('0.6.2.001', '6/1/2012', '254KB', 'mToxGuide.ipa', null);
+$tox_guide_project->add_ios_app($tox_guide_ios_app);
 
 # Wisqars App
-$wisqars_version = '0.2.7';
-$wisqars_release_date = '9/13/13';
-$wisqars_size = '18.5MB';
-$wisqars_ipa = 'WisqarsMobile.ipa';
-$wisqars_app_path = "$downloads_path/wisqars";
-
-$wisqars_manifest_link = "$ios_manifest_prefix$server_domain/$wisqars_app_path/manifest.plist";
-$wisqars_ipa_path = "../$wisqars_app_path/$wisqars_ipa";
+$wisqars_short_desc = 'Allows for sharing injury-related information on a tablet. It dynamically displays selected leading causes of injury death data using maps and charts of national and state-level death counts and rates.';
+$wisqars_project = new Project('wisqars', 'WISQARS Mobile', $wisqars_short_desc, 'images/WISQARSMobileApp72.png');
+$wisqars_ios_app = new IosApp('0.2.7', '9/13/13', '18.5MB', 'WisqarsMobile.ipa', null);
+$wisqars_project->add_ios_app($wisqars_ios_app);
 
 #
 ?>
