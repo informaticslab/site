8f4a8322a81e8a2e0c6fef5f8ade8f3915c8e411
Greg Ledbetter <gsledbetter@gmail.com>
2014-09-11 08:40:53 -0400

    Add gen_manifests, a PHP script to generate manifest files on the server user server name and app meta data.

--- html/applab/mobile_apps_bootstrap.php
+++ html/applab/mobile_apps_bootstrap.php
@@ -11,7 +11,9 @@
 define('SERVER_DOMAIN','phiresearchlab.org');
 #define('SERVER','www'.'.'.SERVER_DOMAIN);  # live
 define('SERVER',''.'172.16.4.156:8082');  # live
-define('DOWNLOADS_PATH_ROOT','/applab/downloads/');
+define('APP_ROOT','/applab/');
+define('DOWNLOADS_RELATIVE_PATH','downloads/');
+
 
 abstract class BaseApp {
     public $version;
@@ -44,11 +46,12 @@ class IosApp extends BaseApp {
         $this->itunes_link = $itunes_link;
     }
 
-    public function set_downloads($downloads_path) {
+    public function set_downloads($downloads_rel_path) {
 
-        $this->ios_dir = "$downloads_path/ios/$this->version/";
-        $this->manifest_link = self::MANIFEST_PREFIX.SERVER.$this->ios_dir.self::MANIFEST_FILE;
-        $this->ipa_path = SERVER.$this->ios_dir.$this->ipa_file;
+
+        $this->ios_dir = $downloads_rel_path.'/ios/'.$this->version.'/';
+        $this->manifest_link = self::MANIFEST_PREFIX.SERVER.APP_ROOT.$this->ios_dir.self::MANIFEST_FILE;
+        $this->ipa_path = SERVER.APP_ROOT.$this->ios_dir.$this->ipa_file;
 
     }
 
@@ -59,46 +62,42 @@ class IosApp extends BaseApp {
             return false;
     }
 
-    public function write_manifest($name, $app_title) {
+    public function write_manifest($app_title) {
 
-        $manifest_path = "downloads/lydia/ios/0.1.3.3/manifest.plist";
-        $manifest_file = fopen($manifest_path, "w") or die("Can't open file: $manifest_path");
+        $manifest_file = fopen($this->ios_dir.self::MANIFEST_FILE, "w") or die("Can't open file: ".$this->ios_dir.self::MANIFEST_FILE);
 
 
-        fwrite($manifest_file, '<?xml version="1.0" encoding="UTF-8"?>');
-        fwrite($manifest_file, '<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">');
-        fwrite($manifest_file,  '<plist version="1.0">');
-        fwrite($manifest_file,  '    <dict>');
-        fwrite($manifest_file,  '        <key>items</key>');
-        fwrite($manifest_file,  '        <array>');
-        fwrite($manifest_file,  '            <dict>');
-        fwrite($manifest_file,  '                <key>assets</key>');
-        fwrite($manifest_file,  '                <array>');
-        fwrite($manifest_file,  '                    <dict>');
-        fwrite($manifest_file,  '                        <key>kind</key>');
-        fwrite($manifest_file,  '                <string>software-package</string>');
-        fwrite($manifest_file,  '                <key>url</key>');
-        fwrite($manifest_file,  '                <string>');
-        fwrite($manifest_file, $this->manifest_link);
-        fwrite($manifest_file, '                </string>');
-        fwrite($manifest_file,  '            </dict>');
-        fwrite($manifest_file,  '        </array>');
-        fwrite($manifest_file,  '        <key>metadata</key>');
-        fwrite($manifest_file,  '        <dict>');
-        fwrite($manifest_file,  '            <key>bundle-identifier</key>');
-        fwrite($manifest_file,  '            <string>');
-        fwrite($manifest_file, $this->bundle_id);
-        fwrite($manifest_file, '             </string>');
-        fwrite($manifest_file,  '            <key>bundle-version</key>');
-        fwrite($manifest_file,  '            <string>');
-        fwrite($manifest_file, $this->version);
-        fwrite($manifest_file, '            </string>');
-        fwrite($manifest_file,  '            <key>kind</key>');
-        fwrite($manifest_file,  '            <string>software</string>');
-        fwrite($manifest_file,  '            <key>title</key>');
-        fwrite($manifest_file,  '            <string>');
-        fwrite($manifest_file, $app_title);
-        fwrite($manifest_file, ' </string></dict></dict></array></dict></plist>');
+        fwrite($manifest_file,  '<?xml version="1.0" encoding="UTF-8"?>'."\n");
+        fwrite($manifest_file,  '<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">'."\n");
+        fwrite($manifest_file,  '<plist version="1.0">'."\n");
+        fwrite($manifest_file,  '  <dict>'."\n");
+        fwrite($manifest_file,  '    <key>items</key>'."\n");
+        fwrite($manifest_file,  '      <array>'."\n");
+        fwrite($manifest_file,  '        <dict>'."\n");
+        fwrite($manifest_file,  '          <key>assets</key>'."\n");
+        fwrite($manifest_file,  '          <array>'."\n");
+        fwrite($manifest_file,  '            <dict>'."\n");
+        fwrite($manifest_file,  '              <key>kind</key>'."\n");
+        fwrite($manifest_file,  '              <string>software-package</string>'."\n");
+        fwrite($manifest_file,  '              <key>url</key>'."\n");
+        fwrite($manifest_file,  '              <string>'.$this->manifest_link."</string>\n");
+        fwrite($manifest_file,  '            </dict>'."\n");
+        fwrite($manifest_file,  '          </array>'."\n");
+        fwrite($manifest_file,  '          <key>metadata</key>'."\n");
+        fwrite($manifest_file,  '          <dict>'."\n");
+        fwrite($manifest_file,  '            <key>bundle-identifier</key>'."\n");
+        fwrite($manifest_file,  '            <string>'.$this->bundle_id."</string>\n");
+        fwrite($manifest_file,  '            <key>bundle-version</key>'."\n");
+        fwrite($manifest_file,  '            <string>'.$this->version."</string>\n");
+        fwrite($manifest_file,  '            <key>kind</key>'."\n");
+        fwrite($manifest_file,  '            <string>software</string>'."\n");
+        fwrite($manifest_file,  '            <key>title</key>'."\n");
+        fwrite($manifest_file,  '            <string>'.$app_title."</string>\n");
+        fwrite($manifest_file,  '          </dict>'."\n");
+        fwrite($manifest_file,  '        </dict>'."\n");
+        fwrite($manifest_file,  '      </array>'."\n");
+        fwrite($manifest_file,  '    </dict>'."\n");
+        fwrite($manifest_file,  '</plist>'."\n");
 
         fclose($manifest_file);
 
@@ -228,23 +227,19 @@ class Project {
         $this->app_title = $title;
         $this->short_description = $short_desc;
         $this->icon = $icon;
-        $this->download_path = DOWNLOADS_PATH_ROOT.$name;
+        $this->download_path = DOWNLOADS_RELATIVE_PATH.$name;
 
 
     }
 
-    public function get_ios_manifest_link() {
+    public function write_ios_manifest_file() {
 
         // make sure manifest.plist file exists first
         if ($this->ios_app->manifest_exists == false) {
 
             // if it does not exist then create it
-            $this->ios_app->write_manifest($this->name, $this->app_title);
+            $this->ios_app->write_manifest($this->app_title);
         }
-
-        $link = $this->ios_app->manifest_link;
-        //die ("Error with manifest link: $link");
-        return $link;
     }
 
 
@@ -391,7 +386,6 @@ class Project {
 # PTT Advisor App
 $ptt_short_desc = 'Assists clinical providers in their evaluation of patients with an abnormal clinical laboratory blood test, specifically an abnormal PTT (Partial Thromboplastin Time).';
 $ptt_project = new Project('ptt-advisor', 'PTT Advisor', $ptt_short_desc, 'images/ptt_icon.png');
-
 $ptt_itunes_link = "https://itunes.apple.com/us/app/ptt-advisor/id537989131?mt=8&ls=1";
 $ptt_ios_app = new IosApp('1.0.3.001', '7/6/12', '1.3MB', 'PTTAdvisor.ipa', $ptt_itunes_link);
 $ptt_project->add_ios_app($ptt_ios_app);
@@ -399,7 +393,6 @@ $ptt_project->add_ios_app($ptt_ios_app);
 # Photon (MMWR Express) App  settings
 $photon_short_desc = 'Provides fast access to the blue summary boxes in MMWR\'s weekly report. Summaries are searchable by specific article, or by specific subject (e.g., salmonella). For iOS devices.';
 $photon_project = new Project('photon', 'MMWR Express', $photon_short_desc, 'images/mmwr_express_icon.png');
-
 $photon_itunes_link = "https://itunes.apple.com/us/app/mmwr-express/id868245971?mt=8";
 $photon_ios_app = new IosApp('1.0.0','5/6/14', '3.2MB', 'photon.ipa', $photon_itunes_link);
 $photon_project->add_ios_app($photon_ios_app);
@@ -417,6 +410,7 @@ $lydia_project->add_android_app($lydia_android_app);
 $clip_short_desc = 'Created in collaboration with National Healthcare Safety Network (NHSN), this app brings the Central Line Insertion Practices Adherence Monitoring form to the iPad.';
 $clip_project = new Project('clip', 'NHSN CLIP', $clip_short_desc, 'images/clip_icon.png');
 $clip_ios_app = new IosApp('0.5.12.001', '6/1/2012', '1.9MB', 'clipam.ipa', null);
+$clip_ios_app->set_bundle_id('gov.cdc.clipam');
 $clip_project->add_ios_app($clip_ios_app);
 
 
@@ -424,6 +418,7 @@ $clip_project->add_ios_app($clip_ios_app);
 $epi_short_desc = 'Created by CDC\'s Epi InfoTM team, this app adapts the StatCalc statistical calculators, a feature of Epi Info desktop software, for the iPad and iPhone.';
 $epi_project = new Project('epi', 'Epi Info', $epi_short_desc, 'images/epi_icon.png');
 $epi_ios_app = new IosApp('2.0', '9/5/14', '15.7MB', 'EpiInfo.ipa', null);
+$epi_ios_app->set_bundle_id('gov.cdc.csels.EpiInfo');
 $epi_project->add_ios_app($epi_ios_app);
 
 
@@ -431,6 +426,7 @@ $epi_project->add_ios_app($epi_ios_app);
 $minesim_short_desc = 'Designed in collaboration with NIOSH (CDC\'s National Institute for Occupational Safety and Health), this proof-of-concept prototype trains mine workers on safety issues.';
 $minesim_project = new Project('minesim', 'NIOSH Mine Safety Training', $minesim_short_desc, 'images/mine_safety_icon.png');
 $minesim_ios_app = new IosApp('0.7301.276', '6/19/2012', '38.8MB', 'mine_sim.ipa', null);
+$minesim_ios_app->set_bundle_id('gov.cdc.MineSim');
 $minesim_project->add_ios_app($minesim_ios_app);
 
 # MMWR Map App
@@ -443,6 +439,7 @@ $mmwr_map_project->add_ios_app($mmwr_map_ios_app);
 $mmwr_nav_short_desc = 'Utilizes the iPad\'s split screen interface to display MMWR content in a user-friendly way. Articles are organized into intuitive categories, making them easy to find.';
 $mmwr_nav_project = new Project('mmwr-navigator', 'MMWR Navigator', $mmwr_nav_short_desc, 'images/mmwr_nav_icon.png');
 $mmwr_nav_ios_app = new IosApp('0.8.11.1', '5/5/14', '10.9MB', 'mmwr-navigator.ipa', null);
+$mmwr_nav_ios_app->set_bundle_id('gov.cdc.mmwr-navigator');
 $mmwr_nav_project->add_ios_app($mmwr_nav_ios_app);
 
 
@@ -450,31 +447,35 @@ $mmwr_nav_project->add_ios_app($mmwr_nav_ios_app);
 $pedigree_short_desc = 'Allows users to record their family health history in one easy-to-reference, centralized place. This app makes it easy to share one\'s family health history with a clinician. For iPhone.';
 $pedigree_project = new Project('pedigree', 'Family Heath History', $pedigree_short_desc, 'images/family_hx_icon.png');
 $pedigree_ios_app = new IosApp('0.4.10.1', '4/15/14', '925KB', 'FamilyHistory.ipa', null);
-$photon_ios_app->set_bundle_id('gov.cdc.FamilyHistory');
+$pedigree_ios_app->set_bundle_id('gov.cdc.FamilyHistory');
 $pedigree_project->add_ios_app($pedigree_ios_app);
 
 # NIOSH Respirator App
 $respguide_short_desc = 'Built in collaboration with The National Institute for Occupational Safety and Health (NIOSH). For quickly exploring the database of NIOSH-approved particulate filtering facepiece respirators.';
 $respguide_project = new Project('respguide', 'NIOSH Facepiece Respirator Guide', $respguide_short_desc, 'images/niosh_face_icon.png');
 $respguide_ios_app = new IosApp('1.2.8.001', '6/4/2012', '321KB', 'Respirator%20Guide.ipa', null);
+$respguide_ios_app->set_bundle_id('gov.CDC.Respirator-Guide');
 $respguide_project->add_ios_app($respguide_ios_app);
 
 # Retro iPad App
 $retro_short_desc = 'Focuses on HIV Risk Assessment — specifically, Assessing your Risk of Contracting HIV (ARCH). This tool is the first in the ARCH suite to be delivered on a mobile platform.';
 $retro_project = new Project('retro', 'ARCH-Couples', $retro_short_desc, 'images/retro_icon.png');
 $retro_ios_app = new IosApp('0.2.1.1', '5/5/14', '957KB', 'retro.ipa', null);
+$retro_ios_app->set_bundle_id('gov.cdc.retro');
 $retro_project->add_ios_app($retro_ios_app);
 
 # STD 1 settings
 $std1_short_desc = 'Early mobile application prototype for CDC\'s 2010 STD Treatment Guidelines. A Reference for clinicians on the identification of and treatment regimen for STDs.';
 $std1_project = new Project('stdguide', 'STD Guide, Version 1', $std1_short_desc, 'images/std1_icon.png');
 $std1_ios_app = new IosApp('0.4.4.001', '6/4/2012', '1.73MB', 'Std-Guide.ipa', null);
+$std1_ios_app->set_bundle_id('gov.cdc.Std-Guide');
 $std1_project->add_ios_app($std1_ios_app);
 
 # STD 2 settings
 $std2_short_desc = 'Enhanced prototype for CDC\'s 2010 STD Treatment Guidelines. A Reference for clinicians on the identification of and treatment regimen for STDs. Version 2 has a more "portal" feel than v1.';
 $std2_project = new Project('std2', 'STD Guide, Version 2', $std2_short_desc, 'images/std2_icon.png');
 $std2_ios_app = new IosApp('0.9.3.001', '6/4/2012', '2.36MB', 'STD%20Guide%202.ipa', null);
+$std2_ios_app->set_bundle_id('gov.CDC.STD-Guide-2');
 $std2_project->add_ios_app($std2_ios_app);
 
 
@@ -490,15 +491,18 @@ $std3_project->add_android_app($std3_android_app);
 $tox_guide_short_desc = 'Quick reference guide provides information such as chemical and physical properties, sources of exposure, minimal risk levels, children\'s health, and health effects.';
 $tox_guide_project = new Project('toxguide', 'ATSDR ToxGuide', $tox_guide_short_desc, 'images/tox_icon.png');
 $tox_guide_ios_app = new IosApp('0.6.2.001', '6/1/2012', '254KB', 'mToxGuide.ipa', null);
+$tox_guide_ios_app->set_bundle_id('gov.cdc.mToxGuide');
 $tox_guide_project->add_ios_app($tox_guide_ios_app);
 
 # Wisqars App
 $wisqars_short_desc = 'Allows for sharing injury-related information on a tablet. It dynamically displays selected leading causes of injury death data using maps and charts of national and state-level death counts and rates.';
 $wisqars_project = new Project('wisqars', 'WISQARS Mobile', $wisqars_short_desc, 'images/WISQARSMobileApp72.png');
 $wisqars_ios_app = new IosApp('0.2.7', '9/13/13', '18.5MB', 'WisqarsMobile.ipa', null);
+$wisqars_ios_app->set_bundle_id('gov.cdc.mToxGuide');
 $wisqars_project->add_ios_app($wisqars_ios_app);
 
 $ios_projects = array (
-    $lydia_project->name => $lydia_project,
-    $pedigree_project->name => $pedigree_project
+    $lydia_project,
+    $pedigree_project,
+    $clip_project
 );
